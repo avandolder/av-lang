995420487fd25b757ef2e869e7317784d855f86e
Adam Vandolder <adam.vandolder@gmail.com>
2018-04-17 14:45:07 -0400

    Added main and make files, modified spec.

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <fstream>
+#include <iostream>
+
+#include "interpreter.h"
+#include "lexer.h"
+#include "parser.h"
+
+int main(int argc, char *argv[])
+{
+    std::ifstream infile(argv[1]);
+
+    Interpreter::interpret(Parser::parse(Lexer::tokenize(infile)));
+
+    return 0;
+}
